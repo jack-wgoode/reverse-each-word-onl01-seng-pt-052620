60c7d43504f0fbdbfe08a3202620a7b385b493a7
Jack Goode <jack.w.goode@icloud.com>
2020-06-07 11:56:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word_collect(string)
-  string_array = string.split(" ")
+  string_array = string.split[" "]
   string_array.collect |word| word.reverse
   string_array.join
 end
